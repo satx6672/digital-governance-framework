f1bf75daedf5051af41b24093562f47b46548dad
DGF V1.0.1 <satx6672@gmail.com>
2026-08-08 21:51:31 -0500

    Delete .spectral.mjs

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMxNzg4Ng/spectral.js?branch_slug=main&token=5886e51e-5aee-40d0-afd3-cb9d83f232eb";
-export default { extends: ruleset };
